dd6738b8fb6cf09808d35d305dca1c4f47d63490
syky <sergiy.kucheryavyy@gft.com>
2022-09-28 17:18:00 +0200

    404 code added

--- src/test/java/TaxCalulationTest.java
+++ src/test/java/TaxCalulationTest.java
@@ -9,6 +9,7 @@ import models.request.Price;
 import models.response.response200.TaxCalculationResponse;
 import models.response.response400.BadRequestResponse;
 import models.response.response401.UnauthorisedResponse;
+import models.response.response404.NotFoundResponse;
 import org.assertj.core.api.Assertions;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -24,29 +25,32 @@ import static io.restassured.RestAssured.given;
 public class TaxCalulationTest extends TestUtils {
     static RequestSpecification calcRequestSpec;
     static RequestSpecification calcRequestSpecInvalid;
+    String path;
     Response responseCalc;
     UnauthorisedResponse unauthorisedResponse;
     BadRequestResponse badRequestResponse;
+    NotFoundResponse notFoundResponse;
     RequestSpecification reqSpecInTest;
 
     @DataProvider(name = "test-data")
     public Object[][] dataProvFunc() {
         return new Object[][]{
-                {true, "EUR", 250, 1027.55, null, 200},
-                {true, "YYY", 250, 0, "currency not found", 400},
-                {false, "EUR", 250, 0, "Unauthorized", 401},
-                {true, "EUR", 500, 1392.05, null, 200}
+//                {true, true, "EUR", 250, 1027.55, null, 200},
+//                {true, true, "YYY", 250, 0, "currency not found", 400},
+//                {false, true, "EUR", 250, 0, "Unauthorized", 401},
+                {true, false, "EUR", 250, 0, "Not Found", 404}  //,
+//                {true, true, "EUR", 500, 1392.05, null, 200}
         };
     }
 
     @BeforeMethod
-    public static void createRequestSpecification()  {
+    public static void createRequestSpecification() {
         calcRequestSpec = given().spec(requestSpecification());
         calcRequestSpecInvalid = given().spec(requestSpecificationInvalid());
     }
 
     @Test(dataProvider = "test-data")
-    public void calculateVat(boolean isRequestValid, String currency, int quantity, double vatExpected, String expectedMessage, int responseCode) throws IOException {
+    public void calculateVat(boolean isRequestValid, boolean isUrlValid, String currency, int quantity, double vatExpected, String expectedMessage, int responseCode) throws IOException {
 
         Price price = new Price(currency, 11.25);
 
@@ -70,9 +74,14 @@ public class TaxCalulationTest extends TestUtils {
         } else {
             reqSpecInTest = calcRequestSpecInvalid;
         }
+        if (isUrlValid) {
+            path = "/v1/shopping-cart";
+        } else {
+            path = "/v1/shopping-cartX";
+        }
 
         responseCalc = given().spec(reqSpecInTest).body(payload)
-                .when().log().all().post("/v1/shopping-cart");
+                .when().log().all().post(path);
 
 //        System.out.println(responseCalc.getBody().jsonPath().prettify());
 
@@ -99,7 +108,7 @@ public class TaxCalulationTest extends TestUtils {
                 Assertions.assertThat(badRequestResponse.getRows().get(0).getMessage()).isEqualTo(expectedMessage);
                 break;
             case 401:
-                responseCalc.then().statusCode(401);
+                responseCalc.then().statusCode(responseCode);
                 unauthorisedResponse = responseCalc.getBody().as(UnauthorisedResponse.class);
                 Assertions.assertThat(unauthorisedResponse.getMessage()).isEqualTo(expectedMessage);
                 break;
@@ -108,6 +117,9 @@ public class TaxCalulationTest extends TestUtils {
                 break;
             case 404:
                 System.out.println("Not Found");
+                responseCalc.then().statusCode(responseCode);
+                notFoundResponse = responseCalc.getBody().as(NotFoundResponse.class);
+                Assertions.assertThat(notFoundResponse.getError()).isEqualTo(expectedMessage);
                 break;
             case 5:
                 System.out.println("Internal Server Error");
